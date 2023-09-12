7463d3b867d9f56d0f1e60f0bd05c50adcd1d10b
Fredde <welandfr@arcada.fi>
2023-09-12 14:55:31 +0300

    mellancommit 2

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
+require('dotenv').config()
+
 
 // disable for production?
-router.get('/', async (req, res) => {
+/*router.get('/', async (req, res) => {
     const users = await prisma.users.findMany()
     console.log("users GET")
     res.send({ 
@@ -23,8 +26,38 @@ router.get('/:id', async (req, res) => {
 
     console.log("users GET ONE")
     res.send({ msg: 'users', user: user })
-})
+}) */
 
+router.post('/login', async (req, res) => {
+    try {
+        const user = await prisma.users.findUnique({
+            where: {email: req.body.email}
+        })
+
+        if (user == null) {
+            return res.status(404).send({msg: 'ERROR', error: 'User not found'})
+        }
+
+        const match = await bcrypt.compare(req.body.password, user.password)
+
+        if (!match) {
+            return res.status(401).send({msg: 'ERROR', error: 'Wrong password'})
+        }
+
+        const token = await jwt.sign({ 
+            sub: user.id, 
+            email: user.email, 
+            name: user.name,
+            expiresIn: '1d'
+        }, process.env.JWT_SECRET)
+
+        res.send({token: token, msg: "Login successful", userId: user.id})
+
+    } catch (error) {
+        
+    }
+
+})
 
 router.post('/', async (req, res) => {
 
@@ -35,20 +68,34 @@ router.post('/', async (req, res) => {
             email: req.body.email,
             name: req.body.name,
             password: hash
-            
         },
     })
     console.log("user created:", user)
     res.send({ msg: 'user created', id: user.id })
 })
 
+/*
 router.patch('/:id', async (req, res) => {
+
+    if (req.params.id != req.authUser.sub) {
+        res.status(403).send({
+            msg: 'ERROR',
+            error: 'Cannot patch other users'
+        })
+    }
+
+    const hash = null
+    if (req.body.password) {
+        hash = await bcrypt.hash(req.body.password, 12)
+    }
 
     const user = await prisma.users.update({
         where: {
             id: req.params.id,
         },
         data: {
+            password: hash,
+            name: req.params.name,
             updatedAt: new Date()
         },
     })
@@ -82,5 +129,6 @@ router.delete('/:id', async (req, res) => {
         })
     }
 })
+*/
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const auth = require('./middleware/auth')
 const PORT = process.env.PORT || 3030
 
-// middleware-funktion
-app.use(auth)
+app.use(cors())
 
 // behövs för att kunna ta emot JSON i request-bodyn
 app.use(express.json())
@@ -14,11 +14,16 @@ app.get('/', (req, res) => {
     res.send('Mainpage!')
 })
 
+const usersRouter = require('./routes/users.js')
+app.use('/users', usersRouter)
+
+// middleware-funktion, validerar jwt
+app.use(auth)
+
 const notesRouter = require('./routes/notes.js')
 app.use('/notes', notesRouter)
 
-const usersRouter = require('./routes/users.js')
-app.use('/users', usersRouter)
+
 
 console.log("Morjens Node!") 
 
